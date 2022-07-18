f17e1bfaf09a8ff44d05b816d0cfce7642b06958
unknown <romanjanofi@gmail.com>
2022-07-18 09:42:19 +0300

    refactor:creatted in another way an object scores

--- let scores = {}.js
+++ let scores = {}.js
@@ -1,8 +1,9 @@
-let scores = {}
-scores["Ben"] = 0
-scores["Bob"] = 0
-scores["Red"] = 0
-scores["Joe"] = 0
+let scores = {
+   "Ben": 0,
+   "Bob": 0,
+   "Red": 0,
+   "Joe": 0
+}
 
 scores.Joe += 3
 scores.Joe += 2
